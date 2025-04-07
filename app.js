11ef3aa910b43939a57a3d30c8596f3ffa39458f
Salokhiddinov Mukhammadali <msalokhiddinov@students.wiut.uz>
2025-04-07 17:45:14 +0500

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const webRoutes = require('./routes/web')
 
 const app = express()
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 app.listen(port, '0.0.0.0', () => {
   console.log(`Server running on port ${port}`);
 });
